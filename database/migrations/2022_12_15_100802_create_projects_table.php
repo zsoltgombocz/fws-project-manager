b0c02b093679af21040ac33a842012d081e2c79a
zsoltgombocz <zsolt.gombocz00@gmail.com>
2022-12-15 14:06:56 +0100

    Updated project table, removed enum, added ondelete set null to contact table

--- database/migrations/2022_12_15_100802_create_projects_table.php
+++ database/migrations/2022_12_15_100802_create_projects_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedInteger('status');
+            $table->text('description');
             $table->timestamps();
         });
     }
--- database/migrations/2022_12_15_100854_create_contacts_table.php
+++ database/migrations/2022_12_15_100854_create_contacts_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->unsignedBigInteger('project_id')->nullable();
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('set null');
             $table->timestamps();
         });
     }
