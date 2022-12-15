622ea5b92a789b510ad87b1d71cf54e0e2271a38
zsoltgombocz <zsolt.gombocz00@gmail.com>
2022-12-15 23:44:32 +0100

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
-    content: ["./resources/js/frontend/**/*.{js,jsx,ts,tsx}"],
+    content: ["resources/js/frontend/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                transparent: "transparent",
+                current: "currentColor",
+                black: colors.black,
+                white: colors.white,
+                gray: colors.slate,
+                green: colors.emerald,
+                purple: colors.violet,
+                yellow: colors.amber,
+                pink: colors.fuchsia,
+                charcoal: "#3A4454",
+                "electric-blue": "#53687E",
+                "red-salsa": "#F05D5E",
+                "red-salsa-500": "#EC3232",
+                alice: "#E7ECEF",
+            },
+            fontFamily: {
+                poppins: ["Poppins", "ui-sans-serif", "system-ui"],
+            },
+        },
     },
+
     plugins: [],
 };
