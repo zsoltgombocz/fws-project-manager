0c58aa3c5fd93cff4786e87172f50ef6264c1ae3
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-09 21:10:58 +0100

    fixed logo overflow + magyarositas

--- resources/js/frontend/navbar/Navbar.jsx
+++ resources/js/frontend/navbar/Navbar.jsx
@@ -9,17 +9,20 @@ const Navbar = () => {
 
     const location = useLocation();
     const isCreateDisabled = location.pathname === '/create';
+    const isBackButtonEnabled = location.pathname !== '/';
 
     return (
         <nav className={'bg-electric-blue w-2/4 flex flex-row p-5 mt-5 rounded-md shadow-xl self-center items-center justify-between'}>
             <div className={'flex flex-row items-center'}>
-                <Link to='/' className={'text-3xl font-medium font mr-2'}>
+                <Link to='/' className={'text-3xl font-medium font mr-2 overflow-y-hidden'}>
                     FWS PM
                 </Link>
-                <span className={'text-gray-400 font-light text-base'}>&#9679; Projects: {projectCount}</span>
+                <span className={'text-gray-400 font-light text-base'}>&#9679; Projektek: {projectCount}</span>
             </div>
-
-            <Link to="/create"><button className={'bg-red-salsa px-3 py-2 rounded-md disabled:opacity-75 disabled:cursor-not-allowed enabled:hover:bg-red-salsa-500'} disabled={isCreateDisabled}>CREATE</button></Link>
+            <div>
+                {isBackButtonEnabled && <Link to="/"><button className={'bg-blue-400 px-3 py-2 rounded-md hover:bg-blue-500 cursor-pointer mr-10'}>VISSZA</button></Link>}
+                <Link to="/create"><button className={'bg-red-salsa px-3 py-2 rounded-md disabled:opacity-75 disabled:cursor-not-allowed enabled:hover:bg-red-salsa-500'} disabled={isCreateDisabled}>LÉTREHOZ</button></Link>
+            </div>
         </nav>
     )
 }
