e7d67092fe1737648210645db24dc4386b99e6fe
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-09 21:13:35 +0100

    list item of a contact, can be used in project and contact form

---
+++ resources/js/frontend/components/ContactListItem.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import EditIcon from '../atom/EditIcon';
+import DeleteIcon from '../atom/DeleteIcon';
+
+const ContactListItem = ({id, name, email, variant, onEditClick, onDeleteClick, isAssigned, checkboxChanged}) => {
+    return (
+    <div className={'mb-2 mt-2 flex flex-row items-center justify-between'}>
+        <label for={`contact-${id}`}>{name} ({email})</label>
+        <div className={'flex gap-5'}>
+        {variant === 'contact' && (
+            <>
+            <EditIcon onClick={onEditClick} />
+            <DeleteIcon onClick={onDeleteClick} />
+            </>
+        )}
+
+        {variant === 'project' && (
+            <label class="inline-flex items-center">
+                <input type="checkbox" className='
+                    rounded
+                    border-gray-300
+                    text-blue-500
+                    shadow-sm
+                    focus:border-indigo-300
+                    focus:ring
+                    focus:ring-offset-0
+                    focus:ring-alice focus:ring-opacity-25
+                ' defaultChecked={isAssigned} onChange={(e) => checkboxChanged(e, id)} id={`contact-${id}`}/>
+            </label>
+        )}
+
+        </div>
+    </div>
+  )
+}
+
+export default ContactListItem
