b1a9ca2c10f5a856c5245e8ba196d865927f5301
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-09 20:48:16 +0100

    removed console logs, added rest of the crud functions

--- resources/js/state/ProjectContext.jsx
+++ resources/js/state/ProjectContext.jsx
@@ -1,5 +1,7 @@
+import { first } from "lodash";
 import { useState, createContext, useEffect } from "react";
 import { api } from "../api/api";
+import { arraysEqual } from "../utils/arraysEqual";
 
 export const ProjectContext = createContext();
 
@@ -17,22 +19,110 @@ export const ProjectProvider = ({ children }) => {
 	const fetchPage = async (page = 1, filter = null) => {
 		try {
 			const { data } = await api().get(`projects?filter=${filter}&page=${page}`);
-			const { next_page_url, prev_page_url, current_page } = data;
+			const { last_page, current_page } = data;
 
 			setCurrentProjects(data.data);
-            console.log(data);
-			setPagination({page: current_page, next: next_page_url, prev: prev_page_url});
+			setPagination({page: current_page, last: last_page, filter});
 		} catch (error) {
-			return new Error(error);
+			throw new Error(error);
 		}
 	};
 
-    const refreshProjectsCount = () => {
-        api().get('projects/?all=true').then(({data}) => setProjectCount(data));
+    const refreshProjectsCount = async () => {
+        try {
+			const { data } = await  api().get('projects/?all=true');
+			setProjectCount(data)
+		} catch (error) {
+			throw new Error(error);
+		}
+    }
+
+    const refreshProjects = () => {
+        refreshProjectsCount();
+        fetchPage(pagination.page, pagination.filter);
+    }
+
+    const getProjectInformation = async (id) => {
+        try {
+			const { data } = await api().get(`projects/${id}`)
+
+            return data;
+		} catch (error) {
+			throw new Error(error);
+		}
+    }
+
+    const createProject = async (payload) => {
+        try {
+			const { data } = await api().post(`projects/create`, payload);
+            //After creating a project the contact list is empty, assign the contacts
+            //based on the payload that we have got
+
+            //pass empty array thus we just created, no contact
+            await addContactsToProject(data.id, payload.contacts, []);
+
+            return data;
+		} catch (error) {
+			throw (error.response.data.errors);
+		}
+    }
+
+    const updateProject = async (id, payload) => {
+        try {
+			const { data } = await api().put(`projects/${id}`, payload);
+            await addContactsToProject(id, payload.contacts, data.contacts);
+
+            return data;
+		} catch (error) {
+			throw (error.response.data.errors);
+		}
+    }
+
+    const deleteProject = async (id) => {
+        try {
+			const { data, status } = await api().delete(`projects/${id}`);
+            if(data === "DELETED" && status === 200) {
+                refreshProjects();
+                return data;
+            }
+		} catch (error) {
+			throw (error);
+		}
+    }
+
+    const addContactsToProject = async (projectId, contacts, defaultContacts) => {
+        try {
+            const defaultIds = defaultContacts.map(dc => dc.id);
+            if(arraysEqual(contacts, defaultIds)) return;
+
+            //first check wheter the already added contacts still added, if not remove them
+            for(let def of defaultIds) {
+                if(!contacts.includes(def)) {
+                    await api().patch(`contacts/assign/${def}/-1`);
+                }
+            }
+
+            //loop through the contact array which contains the updated list of contact ids
+            //we removed the unneccessary contacts, or let them stay as they were, now we
+            //add the rest of the ids listed in the array (making sure the default contact list not including)
+            for(let contact of contacts) {
+                if(!defaultIds.includes(contact)) {
+                    await api().patch(`contacts/assign/${contact}/${projectId}`);
+                }
+            }
+        } catch (error) {
+          throw(error);
+        }
     }
 
 	return (
-		<ProjectContext.Provider value={{ currentProjects, pagination, projectCount, refreshProjectsCount, fetchPage }}>
+		<ProjectContext.Provider
+            value={{
+                currentProjects, pagination,
+                projectCount, refreshProjectsCount,
+                fetchPage, refreshProjects,
+                getProjectInformation, updateProject,
+                createProject, deleteProject }}>
 			{children}
 		</ProjectContext.Provider>
 	);
