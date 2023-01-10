073f3f9250e993f6984d58d6ddf2c351395916d5
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-10 10:13:39 +0100

    removed contact assign function

--- resources/js/state/ProjectContext.jsx
+++ resources/js/state/ProjectContext.jsx
@@ -1,7 +1,5 @@
-import { first } from "lodash";
 import { useState, createContext, useEffect } from "react";
 import { api } from "../api/api";
-import { arraysEqual } from "../utils/arraysEqual";
 
 export const ProjectContext = createContext();
 
@@ -55,11 +53,6 @@ export const ProjectProvider = ({ children }) => {
     const createProject = async (payload) => {
         try {
 			const { data } = await api().post(`projects/create`, payload);
-            //After creating a project the contact list is empty, assign the contacts
-            //based on the payload that we have got
-
-            //pass empty array thus we just created, no contact
-            await addContactsToProject(data.id, payload.contacts, []);
 
             return data;
 		} catch (error) {
@@ -70,7 +63,6 @@ export const ProjectProvider = ({ children }) => {
     const updateProject = async (id, payload) => {
         try {
 			const { data } = await api().put(`projects/${id}`, payload);
-            await addContactsToProject(id, payload.contacts, data.contacts);
 
             return data;
 		} catch (error) {
@@ -88,31 +80,6 @@ export const ProjectProvider = ({ children }) => {
 		} catch (error) {
 			throw (error);
 		}
-    }
-
-    const addContactsToProject = async (projectId, contacts, defaultContacts) => {
-        try {
-            const defaultIds = defaultContacts.map(dc => dc.id);
-            if(arraysEqual(contacts, defaultIds)) return;
-
-            //first check wheter the already added contacts still added, if not remove them
-            for(let def of defaultIds) {
-                if(!contacts.includes(def)) {
-                    await api().patch(`contacts/assign/${def}/-1`);
-                }
-            }
-
-            //loop through the contact array which contains the updated list of contact ids
-            //we removed the unneccessary contacts, or let them stay as they were, now we
-            //add the rest of the ids listed in the array (making sure the default contact list not including)
-            for(let contact of contacts) {
-                if(!defaultIds.includes(contact)) {
-                    await api().patch(`contacts/assign/${contact}/${projectId}`);
-                }
-            }
-        } catch (error) {
-          throw(error);
-        }
     }
 
 	return (
