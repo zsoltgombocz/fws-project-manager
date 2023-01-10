0e30533f6b9d07a5c5c904561e378ba85da5f325
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-10 10:18:03 +0100

    added function to assign contact to project
    passing the project id will assign the contact to that, passing -1 will removes from the current one

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 
 class Contact extends Model
 {
@@ -11,4 +12,14 @@ class Contact extends Model
 
     protected $fillable = ['name', 'email'];
 
+    //passing -1 as id removes from project
+    public function assignToProject($projectId) {
+        if($projectId === -1) {
+            $this->project_id = NULL;
+        }else{
+            $this->project_id = $projectId;
+        }
+
+        $this->save();
+    }
 }
