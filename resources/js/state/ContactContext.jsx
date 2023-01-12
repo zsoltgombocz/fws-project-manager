ca2126c117c24192beeb36eb9590e4acdee9d34d
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-12 12:53:19 +0100

    added the rest of the api routes

--- resources/js/state/ContactContext.jsx
+++ resources/js/state/ContactContext.jsx
@@ -4,24 +4,56 @@ import { api } from "../api/api";
 export const ContactContext = createContext();
 
 export const ContactProvider = ({ children }) => {
-	const [allContact, setAllContact] = useState([]);
+    const [allContact, setAllContact] = useState([]);
 
-	useEffect(() => {
+    useEffect(() => {
         getAllContact();
-	}, []);
+    }, []);
 
-	const getAllContact = async () => {
-		try {
-			const { data } = await api().get(`contacts`);
-			setAllContact(data);
-		} catch (error) {
-			throw new Error(error);
-		}
-	};
+    const getAllContact = async () => {
+        try {
+            const { data } = await api().get(`contacts`);
+            setAllContact(data);
+        } catch (error) {
+            throw new Error(error);
+        }
+    };
 
-	return (
-		<ContactContext.Provider value={{ allContact }}>
-			{children}
-		</ContactContext.Provider>
-	);
+    const createContact = async (payload) => {
+        try {
+            const { data } = await api().post(`contacts/create`, payload);
+            getAllContact();
+            return data;
+        } catch (error) {
+            throw (error.response.data.errors);
+        }
+    }
+
+    const deleteContact = async (id) => {
+        try {
+            const { data, status } = await api().delete(`contacts/${id}`);
+            if (data === "DELETED" && status === 200) {
+                getAllContact();
+                return data;
+            }
+        } catch (error) {
+            throw (error);
+        }
+    }
+
+    const updateContact = async (id, payload) => {
+        try {
+            const { data } = await api().put(`contacts/${id}`, payload);
+            getAllContact();
+            return data;
+        } catch (error) {
+            throw (error.response.data.errors);
+        }
+    }
+
+    return (
+        <ContactContext.Provider value={{ allContact, createContact, deleteContact, updateContact }}>
+            {children}
+        </ContactContext.Provider>
+    );
 };
