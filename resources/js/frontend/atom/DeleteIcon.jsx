6d7f7d484069dc3cf6cc5f81cf8ead38ee7ac257
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-12 13:13:56 +0100

    added disable prop

--- resources/js/frontend/atom/DeleteIcon.jsx
+++ resources/js/frontend/atom/DeleteIcon.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const DeleteIcon = ({ onClick, disabled }) => {
+const DeleteIcon = ({ onClick, disabled = false }) => {
     return (
         <div onClick={disabled ? undefined : onClick} className={`${disabled ? 'text-gray-400' : 'hover:text-red-salsa cursor-pointer inline-block'}`}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
