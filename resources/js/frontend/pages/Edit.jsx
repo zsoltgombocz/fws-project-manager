b24cf4139154d66e936be2c1e7ddbb7a1bef099e
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-12 11:14:15 +0100

    fixed success state initial value

--- resources/js/frontend/pages/Edit.jsx
+++ resources/js/frontend/pages/Edit.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
 import { ProjectContext } from '../../state/ProjectContext';
 import FormSuccess from '../atom/FormSuccess';
@@ -8,17 +8,17 @@ import ProjectForm from '../components/ProjectForm';
 const Edit = () => {
     const { id } = useParams();
     const [project, setProject] = useState(null);
-    const {updateProject, refreshProjects, getProjectInformation} = useContext(ProjectContext)
+    const { updateProject, refreshProjects, getProjectInformation } = useContext(ProjectContext)
     const navigate = useNavigate();
 
     const [errors, setErrors] = useState([]);
-    const [successMessage, setSuccessMessage] = useState([]);
+    const [successMessage, setSuccessMessage] = useState('');
 
     useEffect(() => {
-        if(project === null && !isNaN(id)) {
-            getProjectInformation(id).then(result => setProject(result)).catch(() => navigate('/', {replace: true}));
-        }else{
-            navigate('/', {replace: true});
+        if (project === null && !isNaN(id)) {
+            getProjectInformation(id).then(result => setProject(result)).catch(() => navigate('/', { replace: true }));
+        } else {
+            navigate('/', { replace: true });
         }
     }, [id]);
 
@@ -36,11 +36,11 @@ const Edit = () => {
 
 
     return (
-    <main className={'w-3/4 mx-auto'}>
-        <FormErrors errors={errors} />
-        <FormSuccess message={successMessage} />
-        <ProjectForm projectData={project} onSubmitPressed={handleFormSubmit} create={false} />
-    </main>
+        <main className={'w-3/4 mx-auto'}>
+            <FormErrors errors={errors} onClick={() => setErrors([])} />
+            <FormSuccess message={successMessage} onClick={() => setSuccessMessage('')} />
+            <ProjectForm projectData={project} onSubmitPressed={handleFormSubmit} create={false} />
+        </main>
     )
 }
 
