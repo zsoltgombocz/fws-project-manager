563896f7e1b32d5b5bcbde848f7dab84bc8bd7c8
zsoltgombocz <zsolt.gombocz00@gmail.com>
2023-01-12 11:22:05 +0100

    added onclick prop to be able to hide

--- resources/js/frontend/atom/FormSuccess.jsx
+++ resources/js/frontend/atom/FormSuccess.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const FormSuccess = ({message}) => {
-  return message && message !== "" && message.length > 0 && (
-    <div className={'w-full bg-green-600 border border-1 border-green-900 rounded-md p-2 flex gap-2 flex-col'}>
-        {message}
-    </div>
-  )
+const FormSuccess = ({ message, onClick }) => {
+    return message && message !== "" && message.length > 0 && (
+        <div onClick={onClick} className={'w-full bg-green-600 border border-1 border-green-900 rounded-md p-2 flex gap-2 flex-col cursor-pointer'}>
+            {message}
+        </div>
+    )
 }
 
 export default FormSuccess
--- resources/js/frontend/components/FormErrors.jsx
+++ resources/js/frontend/components/FormErrors.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const FormErrors = ({errors}) => {
+const FormErrors = ({ errors, onClick }) => {
     return errors !== null && errors.length !== 0 &&
-    (<div className={'w-full bg-red-salsa border border-1 border-red-salsa-500 rounded-md p-2 flex gap-2 flex-col'}>
-        {errors.map(err => <div className={''}>{err[1][0]}</div>)}
-    </div>)
+        (<div onClick={onClick} className={'w-full bg-red-salsa border border-1 border-red-salsa-500 rounded-md p-2 flex gap-2 flex-col cursor-pointer'}>
+            {errors.map(err => <div className={''}>{err[1][0]}</div>)}
+        </div>)
 }
 
 export default FormErrors
